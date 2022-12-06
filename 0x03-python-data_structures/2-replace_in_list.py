01cff7e4f3c98b1b8dc32ba4e82cc924f4d3c03d
Joseph Okpako <komeokpako@gmail.com>
2022-12-06 03:18:31 +0100

    task 2

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -4,7 +4,5 @@ def replace_in_list(my_list, idx, element):
     if (idx < 0) or (idx > len(my_list)- 1):
         return(my_list)
 
-    for i in my_list:
-        if i == idx:
-            my_list[i] = element
+    my_list[idx] = element
     return(my_list)
