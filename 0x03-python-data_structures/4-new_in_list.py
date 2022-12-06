7ae8a6f22f5a62fb0e466d24a45add72de1f0f70
Joseph Okpako <komeokpako@gmail.com>
2022-12-06 04:10:01 +0100

    Task 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
 def new_in_list(my_list, idx, element):
-    modified_list = my_list.copy()
-
     if (idx < 0) or (idx > len(my_list) - 1):
-        return(None)
+        modified_list = my_list.copy()
+
     modified_list[idx] = element
     return(modified_list)
