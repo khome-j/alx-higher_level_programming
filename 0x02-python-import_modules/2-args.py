30da39538b227c37a6e5bb2aecbfd0612e439aa4
Joseph Okpako <komeokpako@gmail.com>
2022-12-07 14:04:45 +0100

    Task 2 fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,12 +8,11 @@ if args_len == 0:
     print("0 arguments.")
 elif args_len == 1:
     print("1 argument:")
-    print("{}: {}".format(1, sys.argv[1]))
 else:
     print("{} arguments:".format(args_len))
 
-    for i in range(1, args_len + 1):
-        print("{}: {}".format(i, sys.argv[i]))
+for i in range(1, args_len + 1):
+    print("{}: {}".format(i, sys.argv[i]))
 
 
 
