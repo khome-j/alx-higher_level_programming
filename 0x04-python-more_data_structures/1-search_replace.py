8a7330ffd657b0e53bc469d0a8b25b263409c71a
Joseph Okpako <komeokpako@gmail.com>
2022-12-12 12:34:34 +0100

    task 1

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    new_list = list(my_list)
+    for element in range(len(new_list) - 1):
+        if new_list[element] == search:
+            new_list[element] = replace
+    return(new_list)
