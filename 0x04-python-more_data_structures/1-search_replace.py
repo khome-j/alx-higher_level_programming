3e7f16a7268917f1be02c3afa36c1c514d94f61c
Joseph Okpako <komeokpako@gmail.com>
2022-12-12 12:37:20 +0100

    Task 1 fix

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,7 +2,7 @@
 
 def search_replace(my_list, search, replace):
     new_list = list(my_list)
-    for element in range(len(new_list) - 1):
+    for element in range(len(new_list)):
         if new_list[element] == search:
             new_list[element] = replace
     return(new_list)
