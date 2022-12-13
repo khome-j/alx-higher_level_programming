90ad798a87fdfa17148df17e11c85070e709d694
Joseph Okpako <komeokpako@gmail.com>
2022-12-13 14:27:59 +0100

    TAsk 10 fix

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,7 @@
 def best_score(a_dictionary):
     count = 0
     max = ""
-    if a_dictionary is None:
+    if (a_dictionary is None) or (len(a_dictionary) == 0):
         return None
 
     for key, value in a_dictionary.items():
