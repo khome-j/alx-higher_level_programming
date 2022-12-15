b660b408bdaf4fabac59c0080da2fabe29bdbf58
Joseph Okpako <komeokpako@gmail.com>
2022-12-15 03:37:22 +0100

    Task 15

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def complex_delete(a_dictionary, value):
+    for k in list(a_dictionary):
+        if a_dictionary[k] == value:
+            del a_dictionary[k]
+    return(a_dictionary)
