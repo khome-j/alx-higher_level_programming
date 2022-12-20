d8090b3f0394a1b73f2ed9d772beef73f8e050de
Joseph Okpako <komeokpako@gmail.com>
2022-12-20 14:31:00 +0100

    Task 4

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -5,7 +5,7 @@ def list_division(my_list_1, my_list_2, list_length):
     div = 0
     for i in range(list_length):
         try:
-           div = my_list_1[i] / my_list_2[i]
+            div = my_list_1[i] / my_list_2[i]
         except ZeroDivisionError:
             print("division by 0")
             div = 0
