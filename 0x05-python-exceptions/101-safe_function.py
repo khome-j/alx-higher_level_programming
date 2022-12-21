3ea2f39f65e2c4d630588812f1142de455da7d2f
Joseph Okpako <komeokpako@gmail.com>
2022-12-21 07:06:23 +0100

    Task 8

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+from __future__ import print_function
+
+
+import sys
+
+
+def safe_function(fct, *args):
+    try:
+        return(fct(*args))
+    except Exception as ex:
+        print("Exception: {}".format(ex), file=sys.stderr)
+        return(None)
